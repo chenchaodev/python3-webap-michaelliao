d55c3d81542bc4c5eb2163f0b5121bda5bf7f238
tony <cwtf@Outlook.com>
2015-06-30 13:46:39 +0800

    init

---
+++ dist/orm.py.py
@@ -0,0 +1,105 @@
+import asyncio
+import logging
+import aiomysql
+
+def log(sql, args = ()):
+    logging.info('SQL: %s' &(sql))
+
+@asyncio.coroutine
+def create_pool(loop, **kw):
+    loggin.info('create database connection pool...')
+    global __pool
+    __pool = yield from aiomysql.create_pool {
+        host = kw.get('host', 'locathost'),
+        port = kw.get('port', 3306),
+        user = kw['user'],
+        password = kw['password'],
+        db = kw['db'],
+        charset = kw.get('charset', 'utf8'),
+        autocommit = kw.get('autocommit', True),
+        maxsize = kw.get('maxsize', 10),
+        minsize = kw.get('minsize', 1),
+        loop = loop
+        }
+
+
+@asyncio.coroutine
+def select(sql, args, size = None):
+    log(sql, args)
+    global __pool
+    with (yield from __pool) as conn:
+        cur = yield from conn.cursor(aiomysql.DictCursor)
+        yield from cur.execute(sql.replace('?', '%s'), args or ())
+        if size:
+            rs = yield from cur.fetchmany(size)
+        else:
+            rs = yield from cur.fetchall()
+        yield from cur.close()
+        logging.info('rows returned: %s' % len(rs))
+        return rs
+
+
+@asyncio.coroutine
+def execute(sql, args):
+    log(sql)
+    with (yield from __pool) as conn:
+        try:
+            cur = yield from conn.cursor()
+            yield from cur.execute(sql.replace('?', '%'), args)
+            affected = cur.rowcount
+            yield from cur.close()
+        except BaseException as e:
+            raise
+        return affected
+    
+
+class Model(dict, metaclass = ModelMetaclass):
+    def __init__(self, **kw):
+        super(Model, self).__init__(**kw)
+
+    def __getattr__(self, key):
+        try:
+            return self(key)
+        except KeyError:
+            raise AttributeError(r"'Model' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def getVale(self, key):
+        return getattr(self, key, None)
+
+    def getValueOrDefault(self, key):
+        value = getattr(self, key, None)
+        if value is None:
+            field = self.__mapping__[key]
+            if field.default is not None:
+                value = field.default() if callable(field.default) els field.default
+                logging.debug('using default value for %s: %s' % (kye, str(value)))
+                setattr(self, key, value)
+        return value
+
+
+class Field(object):
+
+    def __init__(self, name, column_type, primary_key, default):
+        self.name = name
+        self,column_type = column_type
+        self,primary_key = primary_key
+        self.default = default
+
+    def __str__(self):
+        return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type, self.name)
+
+
+class StringField(Field):
+    def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
+        super().__init__(name, ddl, primary_key, default)
+
+
+class
+        
+
+
+            
+
